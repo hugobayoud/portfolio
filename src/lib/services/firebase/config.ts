bed47c364c9d2725c06ee191955404a277ae9975
bayoud.hugo@hotmail.com <hugobayoud>
2025-08-06 18:10:40 +0200

    Best handler of secret variables

--- src/lib/services/firebase/config.ts
+++ src/lib/services/firebase/config.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAvGhqOoUyZ26NX1IgIj9Tfhzx82KSaiEk',
-  authDomain: 'portfolio-c336c.firebaseapp.com',
-  databaseURL: 'https://portfolio-c336c-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'portfolio-c336c',
-  storageBucket: 'portfolio-c336c.firebasestorage.app',
-  messagingSenderId: '527681504318',
-  appId: '1:527681504318:web:a561cf6d8187c9e2307d6f',
-  measurementId: 'G-N4V8S4JMN2',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
